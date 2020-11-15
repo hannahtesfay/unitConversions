388e9b7ba795489d5d439b28ff086a88c2756c80
hannahtesfay <hannahltesfay@gmail.com>
2020-11-15 02:49:46 +0000

    log conversion in CLI

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+const conversion = require("../lib/conversion");
+const input = process.argv.splice(2);
+
+const inputNumber = input[0];
+const inputUnit = input[1];
+const outputUnit = input[3];
+
+console.log(conversion.convertInput(inputNumber, inputUnit, outputUnit))
